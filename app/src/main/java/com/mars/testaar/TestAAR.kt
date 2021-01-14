229d44d73b0e1529c304bf869b28113e969de551
geyan <2277087813@qq.com>
2021-01-15 01:27:34 +0800

    hello

--- app/src/main/java/com/mars/testaar/TestAAR.kt
+++ app/src/main/java/com/mars/testaar/TestAAR.kt
@@ -1,11 +1,13 @@
 package com.mars.testaar
 
+import android.util.Log
+
 /**
  * Created by geyan on 2021/1/15
  */
 class TestAAR {
 
     fun hello() {
-
+        Log.e("gy", "发布aar成功了")
     }
 }
